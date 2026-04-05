e3462b05c65c2fc1a5d0867bde191f6f69dc818f
Alessandra <alereis95@gmail.com>
2026-04-05 20:16:59 -0300

    change title screen assets

--- modules/title_screen/title_screen.gd
+++ modules/title_screen/title_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var settings_scene = preload("res://modules/settings/settings.tscn")
 
 func _on_play_pressed() -> void:
 	print("pressed Play")
@@ -7,14 +8,10 @@ func _on_play_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	print("pressed Settings")
-	pass # Replace with function body.
+	var settings = settings_scene.instantiate()
+	add_child(settings)
+	get_tree().paused = true
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-# TODO: create setting menu and connecting to volume slider
-func _on_h_slider_value_changed(value: float) -> void:
-	var bus_index = AudioServer.get_bus_index("Music")
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
