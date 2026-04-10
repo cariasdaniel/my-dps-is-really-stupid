9378811fc729ae8eeb654c725a9ac65bd1786333
Alessandra <alereis95@gmail.com>
2026-04-09 22:15:16 -0300

    move movement (heh) files

--- modules/entity/enemy/enemy_movement.gd
+++ modules/entity/enemy/enemy_movement.gd
@@ -1,8 +1,8 @@
 extends Node
-class_name DpsMovement
+class_name EnemyMovement
 
 @onready var player = $"../../Player"
-@onready var enemy = $"../../Enemy"
+@onready var dps = $"../../Dps"
 
 var direction:= Vector2.ZERO
 var aim_direction:= Vector2.ZERO
@@ -10,12 +10,11 @@ var aim_direction:= Vector2.ZERO
 @export var attack_range:= 175.0
 @export var curr_speed:= 500.0
 
-@onready var safe_area: Area2D = $"../SafeArea"
-
 var entity: CharacterBody2D
 
 func _ready() -> void:
 	entity = get_parent()
+	print("loaded enemy movement")
 
 func set_target(target: CharacterBody2D) -> void:
 	pass
@@ -27,8 +26,8 @@ func _process(delta: float) -> void:
 		pass #play moving
 
 func _on_safe_area_body_entered(body: Node2D) -> void:
+	if body == dps:
+		print("DPS spotted")
 	if body == player:
-		print("Hello friend")
-	if body == enemy:
-		print("Enemy spotted")
+		print("Player spotted")
 	# Switch focus to enemy that just entered
