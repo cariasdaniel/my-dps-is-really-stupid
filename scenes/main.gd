1c22bca1a80a9c829b4f6e5c01171e6af21f17bc
Alessandra <alereis95@gmail.com>
2026-04-09 22:14:31 -0300

    add settings button to main scene

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,3 +7,13 @@ func _process(delta: float) -> void:
 		SignalBus.change_mana.emit(player, 10)
 	if Input.is_action_pressed("lose_health"):
 		SignalBus.change_mana.emit(player, -10)
+
+
+func _on_game_over_button_pressed() -> void:
+	SceneChanger.change_to(ScenePaths.gameOver)
+
+
+func _on_settings_button_pressed() -> void:
+	var settings = load(ScenePaths.settings).instantiate()
+	get_tree().root.add_child(settings)
+	get_tree().paused = true
