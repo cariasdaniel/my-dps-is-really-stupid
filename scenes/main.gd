dc4ec4ff7967a5245fcaef30aa40661bd7686d0b
Alessandra <alereis95@gmail.com>
2026-04-16 22:15:04 -0300

    update sprites

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,12 +2,6 @@ extends Node
 
 @onready var player:= $Player
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("gain_health"):
-		SignalBus.change_mana.emit(player, 10)
-	if Input.is_action_pressed("lose_health"):
-		SignalBus.change_mana.emit(player, -10)
-		
 
 func _on_game_over_button_pressed() -> void:
 	SceneChanger.change_to(ScenePaths.gameOver)
