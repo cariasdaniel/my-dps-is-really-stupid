c1078d307a87dd56968a93d3a8c93f50e9e30f5a
Alessandra <alereis95@gmail.com>
2026-05-16 17:59:17 -0300

    :bug: fix tooltip description

--- modules/tooltip/tooltip.gd
+++ modules/tooltip/tooltip.gd
@@ -4,5 +4,5 @@ func update(skill: SkillData):
 	$MarginContainer/VBoxContainer/Label.text = skill.name
 	$MarginContainer/VBoxContainer/Label2.text = "Cost: %s MP" % skill.cost
 	$MarginContainer/VBoxContainer/Label3.text = "Cooldown: %s sec" % skill.cooldown
-	$MarginContainer/VBoxContainer/Label5.text = skill.description % skill.vars
+	$MarginContainer/VBoxContainer/Label5.text = skill.description % skill.vars.values()
 	
